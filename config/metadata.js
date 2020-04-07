c1ae70cbb2a544804fdfed59eb1d6854422ce0a6
josephdsbr <josephdsbr@gmail.com>
2020-04-07 01:07:27 -0300

    initial commit

---
+++ config/metadata.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'José Vinícius - Portfolio',
+  siteUrl: 'https://www.developer.com',
+  description: 'Portfolio de José Vinícius',
+  social: [
+    {
+      name: 'github',
+      url: 'https://github.com/josephdsbr'
+    },
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/josephdsbr/'
+    }
+  ],
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,50 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const siteMetadata = require('./config/metadata');
+
 module.exports = {
-  /* Your site config here */
+  siteMetadata: siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#7159c1`,
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-98273729",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`300`, `500`, `700`]
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Portfolio - José Vinícius`,
+        short_name: `José Vinícius`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: 'src/assets/icon.png'
+      },
+    },
+    `gatsby-plugin-offline`,
+  ]
 }
